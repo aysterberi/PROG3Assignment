6b9b9ea904e4c3fd9abad6ae69cf4c726de64d07
Joakim Berglund <joakimberglund@live.se>
2016-11-23 10:37:05 +0100

    add createRenderer function

--- GameEngine.h
+++ GameEngine.h
@@ -11,18 +11,20 @@ class GameEngine
 public:
 	struct GameSettings
 	{
-		char *title;
+		char* title;
 		int width;
 		int height;
 	};
     GameEngine();
 	GameEngine(GameSettings game_settings);
-	bool CreateWindow();
+	bool createWindow();
+    SDL_Renderer* createRenderer(SDL_Window* window);
 	~GameEngine();
     bool init();
     SDL_Surface* loadSurface(std::string path);
     SDL_Surface* surface;
     SDL_Window* window;
+    SDL_Renderer* renderer;
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
     const char* WINDOW_TITLE = "Space Invaders";
