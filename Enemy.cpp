8514da5f6f274d3d368f69132100be9f2cdfd0e4
Joakim Berglund <joakimberglund@live.se>
2017-01-08 19:23:02 +0100

    tick method in enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,22 +1,27 @@
 #include "Enemy.h"
+#include "GameEngine.h"
 
 namespace Engine {
+    class GameEngine;
 
-    Enemy::Enemy(SDL_Texture *, SDL_Rect, bool, int dir):
+    Enemy::Enemy(SDL_Texture * texture, SDL_Rect rect, bool drawn = true, int dir = 3) :
         Sprite(texture, rect, drawn), direction(dir) {
-
     }
     Enemy::~Enemy() {
         SDL_DestroyTexture(this->getTexture());
     }
 
-    void Enemy::react(SDL_Event)
-    {
-    }
+    void Enemy::react(SDL_Event) {}
 
     void Enemy::tick(GameEngine &engine) {
         this->incrementRectX(direction);
+        if (this->getRect().x > engine.getScreenWidth()) {
+            this->incrementRectY(5);
+            invertDirection();
+        }
+        else if (this->getRect().x < 0) {
+            this->incrementRectY(5);
+            invertDirection();
+        }
     }
-
-
 }
