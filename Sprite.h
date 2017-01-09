dc19af946badfcc476ac4efa1250c24920a66cd9
Billy <chrysophylax@users.noreply.github.com>
2017-01-10 00:07:14 +0100

    Deleted copy and assignment constructors for Sprite

--- Sprite.h
+++ Sprite.h
@@ -9,24 +9,31 @@ namespace Engine {
 	class GameEngine;
 
 	class Sprite {
-    public:
-        Sprite(SDL_Texture*, SDL_Rect, bool);
-	    virtual ~Sprite();
-        SDL_Texture* getTexture() const { return texture; }
-        SDL_Rect getRect() const { return rect; }
-        void setRectY(int newY) { rect.y = newY; }
-        void setRectX(int newX) { rect.x = newX; }
-        void incrementRectX(int inc);
-        void incrementRectY(int inc);
-        bool isDrawn() const { return drawn; }
+	public:
+		Sprite(SDL_Texture*, SDL_Rect, bool);
+		virtual ~Sprite();
+		SDL_Texture* getTexture() const { return texture; }
+		SDL_Rect getRect() const { return rect; }
+		void setRectY(int newY) { rect.y = newY; }
+		void setRectX(int newX) { rect.x = newX; }
+		void incrementRectX(int inc);
+		void incrementRectY(int inc);
+		bool isDrawn() const { return drawn; }
 		void setDrawn(bool newValue) { drawn = newValue; }
 		virtual void react(SDL_Event event);
 		virtual void tick(GameEngine& engine);
-    private:
-        SDL_Texture* texture;
+
+		//deleting copy, assign constructors 
+		//to prevent value semantics
+		//i.e. enforce pointers and refs
+		Sprite(const Sprite &other) = delete;
+		Sprite& operator=(Sprite const &obj) = delete;
+
+	private:
+		SDL_Texture* texture;
 		SDL_Rect rect;
-        bool drawn;
-    };
+		bool drawn;
+	};
 }
 #endif
 
