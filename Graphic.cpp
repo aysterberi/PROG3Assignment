f963d2e39ac4c86eb74e93d673c8331c12f322a4
Billy <chrysophylax@users.noreply.github.com>
2017-01-10 06:08:23 +0100

    Texture wrapping class. Permits use of smart pointers.

---
+++ Graphic.cpp
@@ -0,0 +1,26 @@
+#include "Graphic.h"
+#include "GameEngine.h"
+#include <iostream>
+
+
+Engine::Graphic::Graphic(SDL_Texture * texture, SDL_Renderer * renderer, int width, int height) :
+	_texture(texture),
+	_renderer(renderer),
+	_width(width),
+	_height(height)
+{
+}
+
+void Engine::Graphic::render(int x, int y)
+{
+	std::cout << "Render on a Graphics object is called\nPosition is x:" << x << "\ty:" << y;
+	SDL_Rect rect = { x,y, _width, _height };
+	SDL_RenderCopy(_renderer, _texture, NULL, &rect);
+}
+
+Engine::Graphic::~Graphic()
+{
+	SDL_DestroyTexture(_texture);
+	_renderer = nullptr;
+}
+
---
+++ Graphic.h
@@ -0,0 +1,22 @@
+#ifndef GRAPHIC_H
+#define GRAPHIC_H
+#include <SDL.h>
+
+namespace Engine {
+	class Graphic
+	{
+	public:
+		Graphic(SDL_Texture*, SDL_Renderer*, int w, int h);
+		int width() { return _width; }
+		int height() { return _height; }
+		void render(int x, int y);
+		SDL_Texture* texture() { return _texture; };
+		~Graphic();
+	private:
+		SDL_Texture* _texture;
+		SDL_Renderer* _renderer;
+		int _width;
+		int _height;
+	};
+}
+#endif
